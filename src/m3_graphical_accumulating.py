ba384b80be6d1fab979cbdd2cb677cc4f1917a3e
brownjw1 <brownjw1@rose-hulman.edu>
2019-03-11 11:42:20 -0400

    DUNZO

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         Aaron Wilkin, their colleagues, and Jared Brown.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -109,6 +109,26 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ###########################################################################
     # -------------------------------------------------------------------------
+    #end=rg.Point(point.x + length, point.y)
+    #start=rg.Point(point.x,point.y)
+    x=point.x
+    x2=point.x+length
+    y=point.y
+    for k in range (n):
+        start=rg.Point(x,y)
+        end=rg.Point(x2,y)
+        line=rg.Line(start,end)
+        line.attach_to(window)
+        #start=start.x,start.y-30
+        #end=end.x,end.y-30
+        y+=30
+        line.attach_to(window)
+    window.render()
+    #window.close_on_mouse_click()
+
+
+
+
 
 
 def run_test_draw_lines():
